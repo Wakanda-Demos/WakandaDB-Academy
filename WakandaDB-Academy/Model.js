ead7feec4c433b60ee6314331d998f6889e7113d
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2013-02-22 13:25:33 +0100

    Update the Model to a JS version supported by WAK4

---
+++ WakandaDB-Academy/Model.js
@@ -0,0 +1,1 @@
+﻿model = {};model.Employee = require('Model/Employee');model.Company = require('Model/Company');model.Country = require('Model/Country');model.Proxy = require('Model/Proxy');
---
+++ WakandaDB-Academy/Modules/Model/Proxy.js
@@ -0,0 +1,1 @@
+﻿var	Proxy;Proxy = exports;// not usedProxy.properties = {    collectionName: 'Proxies'};// Not usedProxy.ID = {    kind: "storage",    type: "long",    autosequence: true,    primKey: true};Proxy.methods = require('Model/Proxy/methods');
